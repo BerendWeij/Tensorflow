607b087adc61b60e84ffe068bbcef8d1dc5d1929
Berend Weij <berend@studiozoetekauw.nl>
2021-02-12 09:16:45 +0100

    updates example

--- basic_draw/js/movingAverage.js
+++ basic_draw/js/movingAverage.js
@@ -1,14 +1,14 @@
 class MovingAverageCalculator {
     constructor(length = 5) {
         this._length = length; // length of moving average
-        this._valueAr = new Array(this._length).fill({x:0, y:0});//movingAverage memory
+        this._values = new Array(this._length).fill({x:0, y:0});//movingAverage memory
     }
     update(newValue) {
-        this._valueAr.pop();//remove the last value
-        this._valueAr.unshift(newValue);//add new value to start
+        this._values.pop();//remove the last value
+        this._values.unshift(newValue);//add new value to start
     }
     get mean() {
-        const sum = this._valueAr.reduce((a, b) => (
+        const sum = this._values.reduce((a, b) => (
             {
                 x: a.x + b.x,
                 y: a.y + b.y
